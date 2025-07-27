d9fade0ae5d3f464cab11280b9afce7cbb338e79
Chakradhar <chakradhar.potluru@codeverse.in>
2025-07-27 22:28:15 +0530

    added accordian

--- src/components/Accordian/Accordian.jsx
+++ src/components/Accordian/Accordian.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Accordian.css'
+import { accordionData } from './accordian-data';
 
-const Accordian = () => {
+const Accordian = () =>{
+  const [selectedAccodian, setSelectedAccodian] = useState(null);
+
+  const handleSelectAccodian = (id) => {
+    console.log("selected id : ", id);
+    setSelectedAccodian(selectedAccodian === id ? null : id);
+  };
+
   return (
-    <div>Accordian</div>
-  )
+    <div className="accodian-container">
+      <div className="accod-header">
+        <h1>Accordian</h1>
+      </div>
+      <div className="accod-main-container">
+        {accordionData.map((item) => {
+          return (
+            <div
+              className="accod-item"
+              key={item.id}
+              onClick={() => handleSelectAccodian(item.id)}
+            >
+              <div
+                className={
+                  selectedAccodian === item.id
+                    ? "accod-main-content  selected-accod"
+                    : "accod-main-content"
+                }
+              >
+                <h3>{item.title}</h3>
+                <span
+                  className={`accod-arrow ${
+                    selectedAccodian === item.id ? "rotate" : ""
+                  }`}
+                >
+                  ▼
+                </span>
+              </div>
+              {selectedAccodian === item.id && (
+                <div className="accod-sub-content">
+                  <h5>{item.content}</h5>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Accordian;
---
+++ src/components/Accordian/accordian-data.js
@@ -0,0 +1,26 @@
+export const accordionData = [
+  {
+    id: 1,
+    title: "What is React?",
+    content:
+      "React is a JavaScript library for building user interfaces. It lets you create reusable UI components.",
+  },
+  {
+    id: 2,
+    title: "What is a Hook in React?",
+    content:
+      "Hooks are functions that let you use state and other React features without writing a class.",
+  },
+  {
+    id: 3,
+    title: "What is the Virtual DOM?",
+    content:
+      "The Virtual DOM is a lightweight copy of the actual DOM. React uses it to improve performance by minimizing direct DOM manipulations.",
+  },
+  {
+    id: 4,
+    title: "How does useEffect work?",
+    content:
+      "`useEffect` runs side-effects in function components, like fetching data, directly manipulating the DOM, etc.",
+  },
+];
